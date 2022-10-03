c4d601a7426c8f9234bc1eda6f98cebb79b1f688
Aleksandr Slukin <aleksandr.slukin@jamf.com>
2022-10-03 16:20:38 +0200

    my first clear try with 2 library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "MyAwesomePackage",
             targets: ["MyAwesomePackage"]),
         .library(name: "MyAwesomePackageDebug",
-                 targets: ["MyAwesomePackageDebug", "MyAwesomePackage"]),
+                 targets: ["MyAwesomePackageDebug"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
